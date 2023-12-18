085f293abc1bd578a43b7094b0acb6904f2c0210
Adnan Majeed <49750395+adnanmajeed82@users.noreply.github.com>
2023-12-18 19:34:56 +0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,30 @@
+# watch_store_api/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="SMART WATCH ONLINE SELLING STORE API",
+      default_version='v1',
+      description="AdnanMajeed API",
+      terms_of_service="https://computerlearningcenter.website2.me",
+      contact=openapi.Contact(email="adnanmajeed82@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('store.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
